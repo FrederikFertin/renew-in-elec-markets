8010b30ed62f4103bf357a8ebbca52e0ba6df63f
thomastrosborg <s203658@student.dtu.dk>
2024-02-15 13:08:51 +0100

    Step 2 started

--- Assignment 1/Step_1-2.py
+++ Assignment 1/Step_1-2.py
@@ -34,14 +34,13 @@ class Network:
 
     # Number of Hours
     TIMES = ['T{0}'.format(t) for t in range(1, 25)]
-    TIMES = ['T1']
 
     # Offer prices indexed by generator
     c = {'G1':13.32,'G2':13.32,'G3':20.7,'G4':20.93,'G5':26.11,'G6':10.52,
          'G7':10.52,'G8':6.02,'G9':5.47,'G10':0,'G11':10.52,'G12':10.89}
     
     # Bid prices indexed by demand
-    u = {'D1':20}
+    u = 400 # $/MWh
 
     # System consumption indexed by time 
     P_D_sum = {'T1': 1775.835,
@@ -69,12 +68,20 @@ class Network:
       'T23': 2517.975,
       'T24': 1669.815}
     
+    # Fraction of system consumption at each node indexed by loads 
+    P_D_fraction = np.array([0.038, 0.034, 0.063, 0.026, 0.025, 0.048, 0.044, 0.06,
+                              0.061, 0.068, 0.093, 0.068, 0.111, 0.035, 0.117, 0.064, 
+                              0.045])
+    
     # Max generation indexed by generator
     P_G_max = {'G1':152,'G2':152,'G3':350,'G4':591, 'G5':60,  'G6':155,
                'G7':155,'G8':400,'G9':400,'G10':300,'G11':310,'G12':350}
     
     # Demand quantities indexed by demand
-    P_D = {'D1':P_D_sum['T1']}
+    P_D = {'D1':P_D_sum}
+    
+    P_R_DW = {k:-v/2 for k,v in P_G_max.items()}
+    P_R_UP = {k:v/2 for k,v in P_G_max.items()}
 
 
 class expando(object):
@@ -85,13 +92,15 @@ class expando(object):
 
 class EconomicDispatch(Network):
     
-    def __init__(self): # initialize class
+    def __init__(self, n_hours, ramping): # initialize class
         # super().__init__(n_samples=n_samples)
         
         self.data = expando() # build data attributes
         self.variables = expando() # build variable attributes
         self.constraints = expando() # build sontraint attributes
         self.results = expando()
+        self.TIMES = self.TIMES[:n_hours]
+        self.ramping = ramping
         self._build_model() # build gurobi model
     
     def _build_model(self):
@@ -99,42 +108,58 @@ class EconomicDispatch(Network):
         self.model = gb.Model(name='Economic Dispatch')
         
         # initialize variables 
-        self.variables.consumption = {d:self.model.addVar(lb=0,ub=self.P_D[d],name='consumption of demand {0}'.format(d)) for d in self.DEMANDS}
-        self.variables.generator_dispatch = {g:self.model.addVar(lb=0,ub=self.P_G_max[g],name='dispatch of generator {0}'.format(g)) for g in self.GENERATORS} 
+        self.variables.consumption = {(d,t):self.model.addVar(lb=0,ub=self.P_D[d][t],name='consumption of demand {0}'.format(d)) for d in self.DEMANDS for t in self.TIMES}
+        self.variables.generator_dispatch = {(g,t):self.model.addVar(lb=0,ub=self.P_G_max[g],name='dispatch of generator {0}'.format(g)) for g in self.GENERATORS for t in self.TIMES} 
         
         # initialize objective to maximize social welfare
-        demand_utility = gb.quicksum(self.u[d] * self.variables.consumption[d] for d in self.DEMANDS)
-        generator_costs = gb.quicksum(self.c[g] * self.variables.generator_dispatch[g] for g in self.GENERATORS)
+        demand_utility = gb.quicksum(self.u * self.variables.consumption[d,t] for d in self.DEMANDS for t in self.TIMES)
+        generator_costs = gb.quicksum(self.c[g] * self.variables.generator_dispatch[g,t] for g in self.GENERATORS for t in self.TIMES)
         objective = demand_utility - generator_costs
         self.model.setObjective(objective, gb.GRB.MAXIMIZE)
         
         # initialize constraints 
-        self.constraints.balance_constraint = self.model.addConstr(
-                gb.quicksum(self.variables.generator_dispatch[g] for g in self.GENERATORS),
+        self.constraints.balance_constraint = {t:self.model.addConstr(
+                gb.quicksum(self.variables.consumption[d,t] for d in self.DEMANDS) - gb.quicksum(self.variables.generator_dispatch[g,t] for g in self.GENERATORS),
                 gb.GRB.EQUAL,
-                gb.quicksum(self.variables.consumption[d] for d in self.DEMANDS),name='Balance equation') # day-ahead balance equation
+                0, name='Balance equation') for t in self.TIMES}
+        
+        T = self.TIMES
+        if self.ramping:
+            self.constraints.ramping_dw = {(g,t):self.model.addConstr(self.variables.generator_dispatch[g,t] - self.variables.generator_dispatch[g,T[n]], 
+                                                               gb.GRB.GREATER_EQUAL, 
+                                                               self.P_R_DW[g]) for g in self.GENERATORS for n,t in enumerate(self.TIMES[1:])}
+            self.constraints.ramping_up = {(g,t):self.model.addConstr(self.variables.generator_dispatch[g,t] - self.variables.generator_dispatch[g,T[n]], 
+                                                               gb.GRB.LESS_EQUAL, 
+                                                               self.P_R_UP[g]) for g in self.GENERATORS for n,t in enumerate(self.TIMES[1:])}
         
     def _save_data(self):
         # save objective value
         self.data.objective_value = self.model.ObjVal
         
         # save consumption values 
-        self.data.consumption_values = {d:self.variables.consumption[d].x for d in self.DEMANDS}
+        self.data.consumption_values = {(d,t):self.variables.consumption[d,t].x for d in self.DEMANDS for t in self.TIMES}
         
         # save generator dispatches 
-        self.data.generator_dispatch_values = {g:self.variables.generator_dispatch[g].x for g in self.GENERATORS}
+        self.data.generator_dispatch_values = {(g,t):self.variables.generator_dispatch[g,t].x for g in self.GENERATORS for t in self.TIMES}
         
         # save uniform prices lambda 
-        self.data.lambda_ = self.constraints.balance_constraint.Pi
+        self.data.lambda_ = {t:self.constraints.balance_constraint[t].Pi for t in self.TIMES}
         
     def run(self):
         self.model.optimize()
         self._save_data()
 
-    def _display_results(self):
+    def calculate_results(self):
+        # calculate profits of suppliers ( profits = (C_G - lambda) * p_G )
+        self.results.profits = {g:sum((self.data.lambda_[t] - self.c[g])  * self.data.generator_dispatch_values[g,t] for t in self.TIMES) for g in self.GENERATORS}
+        
+        # calculate utility of suppliers ( (U_D - lambda) * p_D )
+        self.results.utilities = {d:sum((self.u - self.data.lambda_[t]) * self.data.consumption_values[d,t] for t in self.TIMES) for d in self.DEMANDS}
+    
+    def display_results(self):
         print()
         print("-------------------   RESULTS  -------------------")
-        print("Market clearing price: " + str(np.round(self.data.lambda_, decimals=2)))
+        print("Market clearing prices: " + str(self.data.lambda_))
         print()
         print("Social welfare: " + str(self.data.objective_value))
         print()
@@ -143,22 +168,13 @@ class EconomicDispatch(Network):
         print()
         print("Utility of demands: ")
         print(self.results.utilities)
+        
 
-    def calculate_results(self):
-        # calculate profits of suppliers ( profits = (C_G - lambda) * p_G )
-        self.results.profits = {g:(self.c[g] - self.data.lambda_) * self.data.generator_dispatch_values[g] for g in self.GENERATORS}
-        
-        # calculate utility of suppliers ( (U_D - lambda) * p_D )
-        self.results.utilities = {d:(self.u[d] - self.data.lambda_) * self.data.consumption_values[d] for d in self.DEMANDS}
-        
-        self._display_results()
-        
-        
-    
 if __name__ == "__main__":
-    ec = EconomicDispatch()
+    ec = EconomicDispatch(n_hours=24, ramping=False)
     ec.run()
     ec.calculate_results()
+    ec.display_results()
 
 
 
