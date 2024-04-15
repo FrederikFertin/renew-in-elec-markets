efb5bb13db515369da8c0b1bc5b559a7abcb8585
JacobSkaarup <jacob.skaarup25@gmail.com>
2024-04-15 16:29:09 +0200

    Refactor scenario.py: Updated wind and price scenario generators, added balance scenario generator

--- Assignment 2/scenario.py
+++ Assignment 2/scenario.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
 # Set seed
@@ -25,24 +24,30 @@ prices = prices.iloc[::-1]
 def wind_scenario_generator(n):
     # Generate n random indices which will be used to select n random days
     indices = np.random.choice(int(len(wind.index)/24), n)
-    days = wind.iloc[indices]['time'].dt.date
+    days = wind.iloc[indices*24]['time'].dt.date
     # Select the n random days of data
     scenario = wind.loc[wind['time'].dt.date.isin(days)]
-    print(scenario['offshore'])
     return scenario['offshore'].values.reshape(n, 24)
 
 # Function that selectes n random days from the price data and returns the daily prices for those days
 def price_scenario_generator(n):
     # Generate n random indices which will be used to select n random days
     indices = np.random.choice(int(len(prices.index)/24), n)
-    days = prices.iloc[indices]['HourUTC'].dt.date
+    days = prices.iloc[indices*24]['HourUTC'].dt.date
     # Select the n random days of data
     scenario = prices.loc[prices['HourUTC'].dt.date.isin(days)]
     return scenario['SpotPriceEUR'].values.reshape(n, 24)
 
 
 # Function that generates n scenarios of 24 binary variables based on a bernoulli distribution with p=0.6
-def scenario_generator(n):
+def balance_scenario_generator(n):
     # Generates n scenarios of 24 binary variables based on a bernoulli distribution with p=0.6
 
     return np.random.choice([0, 1], size=(24, n), p=[0.4, 0.6]).T
+
+wind_scenarios = wind_scenario_generator(20)
+price_scenarios = price_scenario_generator(20)
+balance_scenarios = balance_scenario_generator(5)
+scenarios = [[wind_scenarios[i], price_scenarios[j], balance_scenarios[k]] for i in range(20) for j in range(20) for k in range(5)]
+print(np.shape(scenarios))
+print(scenarios[0])
